72feeecfd06ce81063437e7f2435ce90d1255f18
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-08-28 20:03:41 -0300

    declaração das rotas iniciais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PessoaListComponent } from './pessoa/pessoa-list/pessoa-list.component';
+import { PessoaViewComponent } from './pessoa/pessoa-view/pessoa-view.component';
+import { PessoaFormComponent } from './pessoa/pessoa-form/pessoa-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'pessoas/list', component: PessoaListComponent },
+  { path: 'pessoas/view', component: PessoaViewComponent },
+  { path: 'pessoas/novo', component: PessoaFormComponent }
+];
