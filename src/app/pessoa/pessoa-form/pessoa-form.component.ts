0056d5bd0c014b68006b1cab4b1530f92cba43f0
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-08-28 21:01:27 -0300

    declaração de formulario ao inicializar

--- src/app/pessoa/pessoa-form/pessoa-form.component.ts
+++ src/app/pessoa/pessoa-form/pessoa-form.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-pessoa-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './pessoa-form.component.html',
-  styleUrl: './pessoa-form.component.scss'
+  styleUrl: './pessoa-form.component.scss',
 })
-export class PessoaFormComponent {
+export class PessoaFormComponent implements OnInit {
+  pessoaForm: FormGroup = new FormGroup({});
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.pessoaForm = this.fb.group({
+      cpf: ['', [Validators.required]],
+      nome: ['', [Validators.required]],
+      idade: ['', [Validators.required]],
+    });
+  }
 }
