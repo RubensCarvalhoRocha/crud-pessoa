f98243736b3a4f4fdaef5c2be40e3fc7c3b7af8e
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-08-28 21:28:18 -0300

    implementação de formulario na inicialização do component

--- src/app/pessoa/pessoa-form/pessoa-form.component.ts
+++ src/app/pessoa/pessoa-form/pessoa-form.component.ts
@@ -3,13 +3,23 @@ import {
   FormBuilder,
   FormGroup,
   FormsModule,
+  ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-pessoa-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+  ],
   templateUrl: './pessoa-form.component.html',
   styleUrl: './pessoa-form.component.scss',
 })
@@ -23,6 +33,9 @@ export class PessoaFormComponent implements OnInit {
       cpf: ['', [Validators.required]],
       nome: ['', [Validators.required]],
       idade: ['', [Validators.required]],
+      email: ['', [Validators.required]],
     });
   }
+
+  onSubmit() {}
 }
